1b5888923f0aa4e3da8da40b98bd955e0b9878bf
Rodrigo Fogaça <rodrigofogacars@gmail.com>
2023-07-15 01:13:25 -0300

    Códigos da disciplina Programação Orientada a Objetos - arrays

---
+++ 02-programacao-orientada-a-objetos/b-arrays/01-array-length.js
@@ -0,0 +1,26 @@
+// Exemplo de crescimento de arrays e reserva de espaços
+
+var a = []
+a[0] = Math.random()
+console.log(a.length)
+for(let val in a)
+    console.log("  --> " + val)
+
+console.log(" -=-=-=-=-=-=- ")
+
+a.push(Math.random())
+console.log(a.length)
+for(let val in a)
+    console.log("  --> " + val)
+
+console.log(" -=-=-=-=-=-=- ")
+
+a[9] = Math.random()
+console.log(a.length)
+for(let val in a)
+    console.log("  --> " + val)
+
+console.log(" -=-=-=-=-=-=- ")
+
+for(let i=0; i<a.length; i++)
+console.log("  --> " + i + ": " + a[i])
